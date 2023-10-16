0a304175f1f8439e3f9eae40737acc1b87e9d05b
Christian Hartmann <christian.hartmann@qbeyond.de>
2023-10-16 16:27:17 +0200

    completed module with new requirements

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -14,8 +14,30 @@ resource "azurerm_automation_account" "example" {
   sku_name            = "Basic"
 }
 
+resource "azurerm_automation_module" "az_accounts" {
+  name                    = "Az.Accounts"
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = azurerm_automation_account.example.name
+
+  module_link {
+    uri = "https://devopsgallerystorage.blob.core.windows.net:443/packages/az.accounts.2.12.1.nupkg"
+  }
+}
+
+resource "azurerm_automation_module" "az_resourcegraph" {
+  name                    = "Az.Resourcegraph"
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = azurerm_automation_account.example.name
+  module_link {
+    uri = "https://devopsgallerystorage.blob.core.windows.net:443/packages/az.resourcegraph.0.13.0.nupkg"
+  }
+  depends_on = [azurerm_automation_module.az_accounts]
+}
+
 module "update_management" {
   source                     = "../.."
   automation_account         = azurerm_automation_account.example
   management_subscription_id = "abcdef01-2345-6789-0abc-def012345678"
+  management_group_id        = "sandbox"
+  az_resourcegraph_module    = azurerm_automation_module.az_resourcegraph
 }
